c28bffa55075591a32611579a392ce79dd77ea3e
Marcos Souza <marcosfdesouza13@gmail.com>
2025-08-26 21:18:36 -0300

    commit inicial

--- CRUDs/crud_disciplinas.php
+++ CRUDs/crud_disciplinas.php
@@ -1,13 +1,13 @@
-<?php
-  echo("Marcos Souza");
+<?php 
+
 ?>
 
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Calculadora em PHP</title>
+  <title>Document</title>
 </head>
 <body>
   
