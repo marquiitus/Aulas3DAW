3a6b2a331c1a31c094171dd0a0c3857d63543585
Marcos Souza <marcosfdesouza13@gmail.com>
2025-09-20 20:02:18 -0300

    terminando os cruds

--- calculadora/calculadora.php
+++ calculadora/calculadora.php
@@ -1,7 +1,3 @@
-<?php
-  
-?>
-
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -10,6 +6,53 @@
   <title>Calculadora em PHP</title>
 </head>
 <body>
-  
+  <form action="" method="post">
+    <label for="num1">Número 1:</label>
+    <input type="number" id="num1" name="num1" required><br><br>
+
+    <label for="num2">Número 2:</label>
+    <input type="number" id="num2" name="num2" required>
+
+    <label for="operador">Operador:</label>
+    <select id="operador" name="operador" required>
+      <option value="+">+</option>
+      <option value="-">-</option>
+      <option value="*">*</option>
+      <option value="/">/</option>
+    </select> <br><br>
+
+    <button type="submit">Calcular</button>
+  </form>
+
+  <?php
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+      $num1 = $_POST["num1"];
+      $num2 = $_POST["num2"];
+      $operador = $_POST["operador"];
+      $resultado = 0;
+
+      switch($operador) {
+
+        case '+':
+          $resultado = $num1 + $num2;
+          break;
+        case '-':
+          $resultado = $num1 - $num2;
+          break;
+        case '*':
+          $resultado = $num1 * num2;
+          break;
+        case '/':
+          if($num2 != 0) 
+            $resultado = $num1 / $num2;
+          else {
+            echo "não dá para dividir por 0";
+            exit;
+          }
+          break;
+      }
+      echo "<h3>Resultado: $resultado</h3>";
+    }
+  ?>
 </body>
 </html>
