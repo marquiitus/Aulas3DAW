a266f89e81095899e368c4d421e14244f71be1b3
Hugo <souzahugo3273@gmail.com>
2025-11-21 21:34:18 -0300

    Remove .gitignore and add config.php

---
+++ provas/av2/config.php
@@ -0,0 +1,9 @@
+<?php
+
+// Configurações do Banco de Dados
+// Este arquivo deve ser incluído no .gitignore para não ser enviado para o repositório.
+
+$db_host = 'localhost';
+$db_name = 'santa_teresa';
+$db_user = 'root';
+$db_pass = '';
